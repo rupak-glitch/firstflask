bddb0e991d917a03298c3fa10d7c1c99456adaf1
rupak-glitch <singhrupakkp@gmail.com>
2025-09-15 19:05:39 +0530

    my first project

---
+++ firstapp.py
@@ -0,0 +1,44 @@
+from flask import Flask,request,jsonify,render_template
+import pandas as pd
+import pickle
+app=Flask(__name__)
+
+@app.route('/',methods=['GET'])
+def home():
+    return render_template('firstapphtml.html')
+#define your endpoint
+def get_cleaned_data(form_data):
+    gestation=float(form_data['gestation'])
+    parity= int(form_data['parity'])
+    age=float(form_data['age'])
+    height=float(form_data['height'])
+    weight=float(form_data['weight'])
+    smoke=float(form_data['smoke'])
+    cleaned_data={'gestation':gestation,'parity':parity,'age':age,'height':height,'weight':weight,'smoke':smoke}
+    return cleaned_data
+@app.route('/predict',methods=['POST'])
+def get_prediction():
+    #get data from user
+    baby_data_form=request.form
+    baby_data_cleaned=get_cleaned_data(baby_data_form)
+
+    baby_df=pd.DataFrame([baby_data_cleaned])
+
+    #load machine learning model
+
+    with open('model/model.pkl','rb') as f:
+        model=pickle.load(f)
+    
+    #make prediction on user data 
+    prediction=model.predict(baby_df)
+    prediction=round(float(prediction),2)
+
+    #return response in json format
+    response={'prediction':prediction}
+    return render_template('firstapphtml.html', prediction=prediction)
+
+
+
+if __name__== '__main__':
+    app.run(debug=True)
+ 
